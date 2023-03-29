18bd4152161515ad8f7255a852b8212b20c31ad0
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2023-03-29 01:07:46 -0400

    video playlist queue and next video button

--- frontend/src/components/Town/interactables/WatchTogether/PlaylistDrawer.tsx
+++ frontend/src/components/Town/interactables/WatchTogether/PlaylistDrawer.tsx
@@ -11,9 +11,8 @@ import {
   FormControl,
   FormLabel,
   Input,
-  InputGroup,
   Button,
-  InputRightElement,
+  FormErrorMessage,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
 
@@ -30,6 +29,22 @@ export default function PlaylistDrawer({
 }): JSX.Element {
   const [inputVideoURL, setInputVideoURL] = useState<string>('');
 
+  const regExpURL =
+    /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+
+  const isInValidYoutubeURL = !regExpURL.test(inputVideoURL) && !(inputVideoURL === '');
+
+  const handleYotubeVideoURL = () => {
+    // check if it starts with youtube.com/youtu.be and has video id after v= with 11 digits id.
+    if (!isInValidYoutubeURL && !playList.includes(inputVideoURL)) {
+      // if matches, update the video playlist
+      const newPlaylist = [...playList];
+      newPlaylist.push(inputVideoURL);
+      handlePlaylistUpdate(newPlaylist);
+      setInputVideoURL('');
+    }
+  };
+
   return (
     <Drawer
       isOpen={drawerIsOpen}
@@ -40,36 +55,28 @@ export default function PlaylistDrawer({
       <DrawerOverlay />
       <DrawerContent>
         <DrawerCloseButton />
-        <DrawerHeader>Video playlist</DrawerHeader>
+        <DrawerHeader>Control panel</DrawerHeader>
 
         <DrawerBody>
           <List spacing={3}>
             {/* Video URL input box */}
-            <FormControl>
+            <FormControl isInvalid={isInValidYoutubeURL}>
               <FormLabel htmlFor='video'>Video URL</FormLabel>
-              <InputGroup>
-                <InputRightElement>
-                  <Button
-                    colorScheme='teal'
-                    onClick={() => {
-                      const newPlaylist = [...playList];
-                      newPlaylist.push(inputVideoURL);
-                      handlePlaylistUpdate(newPlaylist);
-                      console.log(playList);
-                    }}
-                    inlineSize={'full'}>
-                    +
-                  </Button>
-                </InputRightElement>
-                <Input
-                  id='video'
-                  name='video'
-                  value={inputVideoURL}
-                  onChange={e => {
-                    setInputVideoURL(e.target.value);
-                  }}
-                />
-              </InputGroup>
+              <Input
+                id='video'
+                name='video'
+                value={inputVideoURL}
+                placeholder={'Paste youtube video url here'}
+                onChange={e => {
+                  setInputVideoURL(e.target.value);
+                }}
+                onKeyPress={e => {
+                  if (e.key === 'Enter') {
+                    handleYotubeVideoURL();
+                  }
+                }}
+              />
+              <FormErrorMessage>Please input a valid Youtube video URL</FormErrorMessage>
             </FormControl>
 
             {/* Next video play button */}

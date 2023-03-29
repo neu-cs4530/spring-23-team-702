7f27a4788cf751393610ad8daa5b371c8ce67c61
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2023-03-29 12:57:29 -0400

    convert into function

--- townService/src/api/YoutubeAPI.ts
+++ townService/src/api/YoutubeAPI.ts
@@ -1,33 +1,37 @@
-import assert from 'assert';
 import axios from 'axios';
 
 const APIURL = 'https://www.googleapis.com/youtube/v3/';
 
-export default class API {
-  private readonly _apiKey: string;
-
-  public constructor() {
-    const key = process.env.YOUTUBE_API_KEY;
-    assert(key);
-    this._apiKey = key;
+function getVideoID(videoURL: string): string {
+  const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  const match = videoURL.match(regExp);
+  if (match && match[7].length === 11) {
+    return match[7];
   }
+  return '';
+}
 
-  public getVideoDetail = async (videoURL: string) => {
-    const endpoint = 'videos';
-    const params = {
-      part: 'snippet',
-      key: this._apiKey,
-      id: videoURL,
-    };
-    const response = await axios.get(APIURL + endpoint, { params }).then(r => r.data);
+export default async function getVideoDetail(videoURL: string): Promise<{
+  thumbnails: string;
+  title: string;
+}> {
+  const apiKey = process.env.YOUTUBE_API_KEY;
+  const videoID = getVideoID(videoURL);
 
-    if (!response.items[0]) return Promise.reject(new Error('Nothing was found.'));
+  const params = {
+    part: 'snippet',
+    key: apiKey,
+    id: videoID,
+  };
 
-    const { snippet } = response.items[0];
+  const response = await axios.get(`${APIURL}videos`, { params }).then(r => r.data);
 
-    return {
-      thumbnails: snippet.thumbnails.default.url,
-      title: snippet.title,
-    };
+  if (!response.items[0]) return Promise.reject(new Error('Nothing was found.'));
+
+  const { snippet } = response.items[0];
+
+  return {
+    thumbnails: snippet.thumbnails.default.url,
+    title: snippet.title,
   };
 }
